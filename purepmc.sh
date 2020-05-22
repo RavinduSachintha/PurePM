793fcf6884707044961314fb35a6b204cb49fbd3
RavinduSachintha <ravindusachintha53@gmail.com>
2020-05-22 09:34:03 +0530

    purepmc updated for init process

--- purepmc.sh
+++ purepmc.sh
@@ -1,27 +1,60 @@
-echo "####### Pure-PM C-Client #######"
-echo
+# constants
+DEFCMD='null'
 
-echo "What do you need ?"
-echo -e " - 1. compile(object files)"
-echo -e " - 2. executable"
-echo -e " - 3. clean"
+# global variables: CMD, PWD
 
-read -p "Your choice : " varChoice
+# main function
+main() {
+	CMD=$1
+	PWD=$('pwd')
+	checkCommands $CMD
+}
 
-echo "--------------------------------"
+# check input commands
+checkCommands() {
+	if [ $CMD == 'init' ]
+	then 
+		init
+	fi
+}
 
-if [ $varChoice -eq '1' ]
-then 
+# init process
+init() {
+	echo "****** PurePM C-Client ******"
+	echo "-----------------------------"
+	echo " - Project initilizing at $PWD"
+	mkdir pure_pkgs
+	mkdir src
+	mkdir dist
+}
+
+# program
+main ${1:-$DEFCMD}
+
+#echo "####### Pure-PM C-Client #######"
+#echo
+
+#echo "What do you need ?"
+#echo -e " - 1. compile(object files)"
+#echo -e " - 2. executable"
+#echo -e " - 3. clean"
+
+#read -p "Your choice : " varChoice
+
+#echo "--------------------------------"
+
+#if [ $varChoice -eq '1' ]
+#then 
 #	make compile
-	echo Hello
-elif [ $varChoice -eq '2' ]
-then 
+#	echo Hello
+#elif [ $varChoice -eq '2' ]
+#then 
 #	make exec
-	echo Hello
-elif [ $varChoice -eq '3' ]
-then 
+#	echo Hello
+#elif [ $varChoice -eq '3' ]
+#then 
 #	make clean
-	echo Hello
-else 
-	echo 'Please, make a correct a choice :-)'
-fi
+#	echo Hello
+#else 
+#	echo 'Please, make a correct a choice :-)'
+#fi
