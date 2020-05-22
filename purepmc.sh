2631078aedf90c84fadb1d5171f908e43a3834f2
Restyled.io <commits@restyled.io>
2020-05-22 06:33:46 +0000

    Restyled by shfmt

--- purepmc.sh
+++ purepmc.sh
@@ -6,33 +6,31 @@ BASE_URL='https://raw.githubusercontent.com/RavinduSachintha/PurePM/master/base/
 
 # main function
 main() {
-	CMD=$1
-	PWD=$('pwd')
-	checkCommands "$CMD"
+  CMD=$1
+  PWD=$('pwd')
+  checkCommands "$CMD"
 }
 
 # check input commands
 checkCommands() {
-	if [ "$CMD" == 'init' ]
-	then 
-		init
-	fi
+  if [ "$CMD" == 'init' ]; then
+    init
+  fi
 }
 
 # init process
 init() {
-	echo "****** PurePM C-Client ******"
-	echo "-----------------------------"
-	echo " - Project initilizing at $PWD"
-	curl -LO "${BASE_URL}Makefile"
-	mkdir pure_pkgs && cd pure_pkgs
-	curl -LO "${BASE_URL}pure_pkgs/Makefile"
-	cd .. && mkdir src && cd src
-	curl -LO "${BASE_URL}src/Makefile"
-	curl -LO "${BASE_URL}src/main.c"
-	cd .. && mkdir dist
+  echo "****** PurePM C-Client ******"
+  echo "-----------------------------"
+  echo " - Project initilizing at $PWD"
+  curl -LO "${BASE_URL}Makefile"
+  mkdir pure_pkgs && cd pure_pkgs
+  curl -LO "${BASE_URL}pure_pkgs/Makefile"
+  cd .. && mkdir src && cd src
+  curl -LO "${BASE_URL}src/Makefile"
+  curl -LO "${BASE_URL}src/main.c"
+  cd .. && mkdir dist
 }
 
 # program
 main "${1:-$DEFCMD}"
-
