88eb55db71349039c3d24a9a3209787f6bf62625
Sachintha <ravindusachintha53@gmail.com>
2020-05-22 12:04:25 +0530

    fixed conflicts (#4)
    * restructured
    
    * purepmc updated for init process
    
    * base creation implemented for init

--- purepmc.sh
+++ purepmc.sh
@@ -1,5 +1,6 @@
 # constants
 DEFCMD='null'
+BASE_URL='https://raw.githubusercontent.com/RavinduSachintha/PurePM/master/base/'
 
 # global variables: CMD, PWD
 
@@ -23,38 +24,15 @@ init() {
 	echo "****** PurePM C-Client ******"
 	echo "-----------------------------"
 	echo " - Project initilizing at $PWD"
-	mkdir pure_pkgs
-	mkdir src
-	mkdir dist
+	curl -LO "${BASE_URL}Makefile"
+	mkdir pure_pkgs && cd pure_pkgs
+	curl -LO "${BASE_URL}pure_pkgs/Makefile"
+	cd .. && mkdir src && cd src
+	curl -LO "${BASE_URL}src/Makefile"
+	curl -LO "${BASE_URL}src/main.c"
+	cd .. && mkdir dist
 }
 
 # program
 main ${1:-$DEFCMD}
 
-#echo "####### Pure-PM C-Client #######"
-#echo
-
-#echo "What do you need ?"
-#echo -e " - 1. compile(object files)"
-#echo -e " - 2. executable"
-#echo -e " - 3. clean"
-
-#read -p "Your choice : " varChoice
-
-#echo "--------------------------------"
-
-#if [ $varChoice -eq '1' ]
-#then 
-#	make compile
-#	echo Hello
-#elif [ $varChoice -eq '2' ]
-#then 
-#	make exec
-#	echo Hello
-#elif [ $varChoice -eq '3' ]
-#then 
-#	make clean
-#	echo Hello
-#else 
-#	echo 'Please, make a correct a choice :-)'
-#fi
